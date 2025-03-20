a4c2c2912f152ec0b42011373d6d9d0cb2dd2b17
João <joaoaraujo2001@hotmail.com>
2025-03-20 23:30:13 +0000

    GH PAGES V4

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,38 @@
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// // import NavBar from "./components/Navbar";
+// import Home from "./components/Home";
+// import Estudio from "./components/Estudio";
+// import Galeria from "./components/Galeria";
+// import Informacoes from "./components/Informacoes";
+// import Contactos from "./components/Contactos";
+// import Blog from "./components/Blog";
+// import BlogPost from "./components/BlogPost";
+
+// function App() {
+//   return (
+//     <div className="min-h-full font-Raleway">
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           {/* <Route path="/pagina-inicial" element={<Home />} /> */}
+//           <Route path="/estudio" element={<Estudio />} />
+//           <Route path="/galeria" element={<Galeria />} />
+//           <Route path="/informacoes" element={<Informacoes />} />
+//           <Route path="/contactos" element={<Contactos />} />
+//           <Route path="/blog" element={<Blog />} />
+//           <Route path="/blog/:slug" element={<BlogPost />} />
+//         </Routes>
+//       </BrowserRouter>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// import NavBar from "./components/Navbar";
+// Importando os componentes das páginas
 import Home from "./components/Home";
 import Estudio from "./components/Estudio";
 import Galeria from "./components/Galeria";
@@ -12,10 +44,10 @@ import BlogPost from "./components/BlogPost";
 function App() {
   return (
     <div className="min-h-full font-Raleway">
-      <BrowserRouter>
+      {/* Adicionando o basename */}
+      <BrowserRouter basename="/oopsiinkedagain">
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="/pagina-inicial" element={<Home />} /> */}
           <Route path="/estudio" element={<Estudio />} />
           <Route path="/galeria" element={<Galeria />} />
           <Route path="/informacoes" element={<Informacoes />} />
