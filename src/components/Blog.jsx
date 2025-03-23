fac314bfe10b26d7d2f900243d75c37ac8aefed7
João Araújo <joaoaraujo2001@hotmail.com>
2025-03-23 23:54:52 +0000

    test24

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -16,11 +16,11 @@ function Estudio() {
   const noticias = [
     {
       titulo: "Cuidados a ter com a pele",
-      imagem: "/oopsiinkedagain/assets/image1.png",
+      imagem: "/assets/image1.png",
     },
     {
       titulo: "Dicas...",
-      imagem: "/oopsiinkedagain/assets/image2.png",
+      imagem: "/assets/image2.png",
     },
   ];
 
@@ -66,7 +66,7 @@ function Estudio() {
                   {noticia.titulo}
                 </h1>
                 <a
-                  href={`/oopsiinkedagain/blog/${slugify(noticia.titulo)}`}
+                  href={`/blog/${slugify(noticia.titulo)}`}
                   className="outline-none border-none rounded-[5rem] uppercase text-[13px] sm:text-[15px] cursor-pointer mt-[20px] sm:mt-[30px] text-white transition duration-200 flex items-center justify-start gap-5 no-underline"
                 >
                   Saiba mais
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,9 +11,9 @@ import ScrollTop from "./ScrollTop";
 // import Loader from "./Loader";
 
 const images = [
-  "/oopsiinkedagain/assets/tattoo1.png",
-  "/oopsiinkedagain/assets/tattoo2.png",
-  "/oopsiinkedagain/assets/tattoo3.png",
+  "/assets/tattoo1.png",
+  "/assets/tattoo2.png",
+  "/assets/tattoo3.png",
 ];
 
 function Home() {
@@ -57,7 +57,7 @@ function Home() {
   return (
     <div
       className="relative h-screen bg-cover bg-center"
-      style={{ backgroundImage: "url(/oopsiinkedagain/assets/image1.png)" }}
+      style={{ backgroundImage: "url(/assets/image1.png)" }}
     >
       {/* Navbar fora do fluxo do layout principal */}
       <NavBar
@@ -173,7 +173,7 @@ function Home() {
                 <a
                   className="text-[#FFFFFF] uppercase mx-4"
                   style={{ fontFamily: "'Source Serif 4', serif" }}
-                  href="/oopsiinkedagain/estudio"
+                  href="/estudio"
                 >
                   Leia a nossa história
                 </a>
@@ -200,14 +200,14 @@ function Home() {
           <div className="relative">
             {/* Primeira imagem */}
             <img
-              src="/oopsiinkedagain/assets/image2.png"
+              src="/assets/image2.png"
               alt="Estúdio"
               className="max-w-[70%] sm:max-w-xs md:max-w-sm lg:max-w-md"
             />
 
             {/* Segunda imagem, posicionada absolutamente sobre a primeira */}
             <img
-              src="/oopsiinkedagain/assets/image3.png"
+              src="/assets/image3.png"
               alt="Estúdio"
               className="absolute top-[16rem] left-[8rem] max-w-[70%] sm:max-w-xs md:max-w-sm lg:max-w-md"
               // className="max-w-full mt-4 md:absolute md:top-1/2 md:left-1/2 md:transform md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[60%]"
@@ -232,7 +232,7 @@ function Home() {
             <a
               className="text-[#727171] uppercase mx-4"
               style={{ fontFamily: "'Source Serif 4', serif" }}
-              href="/oopsiinkedagain/galeria"
+              href="/galeria"
             >
               Mostrar mais
             </a>
@@ -242,14 +242,14 @@ function Home() {
             {/* Primeira linha com as imagens 1 e 2 */}
             <div className="flex justify-center">
               <img
-                src="/oopsiinkedagain/assets/tattoo1.png"
+                src="/assets/tattoo1.png"
                 alt="Imagem 1"
                 className="w-full h-[230px] object-cover md:rounded-tl-3xl"
               />
             </div>
             <div className="flex justify-center">
               <img
-                src="/oopsiinkedagain/assets/tattoo2.png"
+                src="/assets/tattoo2.png"
                 alt="Imagem 2"
                 className="w-full h-[230px] object-cover"
               />
@@ -258,7 +258,7 @@ function Home() {
             {/* Terceira linha com a imagem 5 ocupando as linhas 2 e 4 */}
             <div className="flex justify-center md:col-span-1 md:row-span-3">
               <img
-                src="/oopsiinkedagain/assets/tattoo5.png"
+                src="/assets/tattoo5.png"
                 alt="Imagem 5"
                 className="w-full h-[230px] md:h-[468px] object-cover md:rounded-tr-3xl md:rounded-br-3xl"
               />
@@ -267,14 +267,14 @@ function Home() {
             {/* Segunda linha com as imagens 3 e 4 */}
             <div className="flex justify-center">
               <img
-                src="/oopsiinkedagain/assets/tattoo3.png"
+                src="/assets/tattoo3.png"
                 alt="Imagem 3"
                 className="w-full h-[230px] object-cover md:rounded-bl-3xl"
               />
             </div>
             <div className="flex justify-center">
               <img
-                src="/oopsiinkedagain/assets/tattoo4.png"
+                src="/assets/tattoo4.png"
                 alt="Imagem 4"
                 className="w-full h-[230px] object-cover"
               />
@@ -287,7 +287,7 @@ function Home() {
       <section className="bg-[#101010] pt-[150px] pb-[70px]" id="tattoos">
         <div
           className="relative h-[25vh] bg-cover bg-center"
-          style={{ backgroundImage: "url(/oopsiinkedagain/assets/background1.png)" }}
+          style={{ backgroundImage: "url(/assets/background1.png)" }}
         >
           <h2
             className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-2xl sm:text-3xl md:text-4xl lg:text-4xl uppercase font-bold w-full text-center"
@@ -363,7 +363,7 @@ function Home() {
         </div>
         <div className="relative z-10 flex flex-col justify-center items-center h-full text-center text-white">
           <a
-            href="/oopsiinkedagain/informacoes"
+            href="/informacoes"
             className="mt-12 px-6 py-3 border-2 border-white text-white uppercase hover:bg-white hover:text-black transition duration-700"
           >
             Mostrar mais
@@ -444,7 +444,7 @@ function Home() {
             <div
               className="relative h-[45%] bg-cover bg-center flex items-baseline justify-between p-6 md:px-[50px]"
               style={{
-                backgroundImage: "url('/oopsiinkedagain/assets/background1.png')",
+                backgroundImage: "url('/assets/background1.png')",
                 boxShadow: "inset 0 10px 10px rgba(9, 9, 9, 100%)",
               }}
             >
