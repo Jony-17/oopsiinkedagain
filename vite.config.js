7758e6df270384dc6dc0a55468d179d2e485db55
João Araújo <joaoaraujo2001@hotmail.com>
2025-03-23 23:46:49 +0000

    test22

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/oopsiinkedagain/",
+  base: "/",
   build: {
     outDir: "dist", // Certifique-se de que a saída seja para 'dist'
   },
